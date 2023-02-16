c91b298a78d115065e90f8a6a80bdd7cc3e1c340
anel <98648578+anelaktasova@users.noreply.github.com>
2023-02-16 23:09:06 +0600

    Add files via upload

---
+++ lab3/javainfo/Data_types/Array methods/Create keyed object from array.js
@@ -0,0 +1,27 @@
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+  ];
+  
+  let usersById = groupById(users);
+  
+  /*
+  // after the call we should have:
+  
+  usersById = {
+    john: {id: 'john', name: "John Smith", age: 20},
+    ann: {id: 'ann', name: "Ann Smith", age: 24},
+    pete: {id: 'pete', name: "Pete Peterson", age: 31},
+  }
+  */
+
+
+
+// solution:
+function groupById(array) {
+    return array.reduce((obj, value) => {
+      obj[value.id] = value;
+      return obj;
+    }, {})
+  }
---
+++ lab3/javainfo/Data_types/Object.keys, values, entries/Count properties.js
@@ -0,0 +1,12 @@
+let user = {
+    name: 'John',
+    age: 30
+  };
+  
+  alert( count(user) ); // 2
+
+
+
+  function count(obj) {
+    return Object.keys(obj).length;
+  }
