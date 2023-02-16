a9de4dfa1d982c8eace22c99213b5d66acc28b92
anel <98648578+anelaktasova@users.noreply.github.com>
2023-02-16 21:32:28 +0600

    Add files via upload
    lab3

---
+++ lab3/aqtassova/todo.js
@@ -0,0 +1,83 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+    list.appendChild(checkbox);
+  }
+}, false);
+
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////
+
+// let addToDoButton = document.getElementById('addbutton');
+// let todocontain = document.getElementById('ToDoContain');
+// let inputtxt = document.getElementById('inputtxt');
+
+
+
+
+// addToDoButton.addEventListener('click', function(){
+// var paragraph = document.createElement('p');
+// let checkbox = document.createElement('input'); //creating input for checkbox
+// checkbox.setAttribute("type", "checkbox"); // setting attribute to make it chackbox
+// checkbox.setAttribute("onclick", "editStyle()");
+// checkbox.setAttribute("id", 'checkbutton');
+// paragraph.innerText = inputtxt.value; 
+
+// if (inputtxt.value == "")
+// {
+//     alert("You must write something")
+// }
+// else{
+//     todocontain.appendChild(paragraph);
+//     paragraph.appendChild(checkbox);
+//     paragraph.appendChild(img);
+//     inputtxt.value = ""
+// }
+
+
+// })
